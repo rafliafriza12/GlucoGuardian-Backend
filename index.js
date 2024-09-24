967ee382da1f814ee5b0ce7832339038f8256b86
rafliafriza12 <rafliafriza90@gmail.com>
2024-09-25 04:38:21 +0700

    initial commit

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
+import { configDotenv } from "dotenv";
 import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 5000;
 
-app.get('/', (req, res) => {
-    res.json({
-        "status": 200,
-        "message": "request ok"
-    })
-})
+configDotenv();
+
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+async function run() {
+  try {
+    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+    await mongoose.connect(process.env.MONGO_URI, clientOptions);
+    await mongoose.connection.db.admin().command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await mongoose.disconnect();
+  }
+}
+run().catch(console.dir);
+
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.listen(port, () => {
-    console.log(`server running on ${port}`);
-})
+    console.log(`Server running on port ${port}`);
+});
